bcf00ff84ce28deaa8fd6cc35bed3ae9b8bcf11b
Karemu1842call <victorkinoti03@gmail.com>
2022-06-20 12:12:30 +0300

    commit tree 01392b2e48a6ebcd2411606e67c1e52d857a64f3

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+"use strict";
+function submitData(_name_, _email_) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then((_response_) => {
+      return _response_.json();
+    })
+    .then((_object_) => {
+      document.body.innerHTML = _object_["id"];
+    })
+    .catch((_error_) => {
+      document.body.innerHTML = _error_.message;
+    });
+};
+submitData("Victor", "anonymous@gmail.com");
